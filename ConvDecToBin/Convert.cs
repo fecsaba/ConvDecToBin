9f876674b3584de82efca83c7dc247ae14a8d024
Csaba Fehér <nexusegyesulet@gmail.com>
2018-01-19 17:08:51 +0100

    v0.1

--- ConvDecToBin/Convert.cs
+++ ConvDecToBin/Convert.cs
@@ -8,5 +8,42 @@ namespace ConvDecToBin
 {
     class Convert
     {
+        string dec;
+
+        public string Dec
+        {
+            get
+            {
+                return dec;
+            }
+            set
+            {
+                dec = DToB(value);
+            }
+        }
+        public string DToB(string st)
+        {
+            
+            bool ok = int.TryParse(st, out int num);
+            //int num = int.Parse(st);
+            string result = "";
+            if (ok)
+            {
+                while (num > 0)
+                {
+                    int remainder = num % 2;
+                    result = remainder.ToString() + result;
+
+                    num /= 2;
+                }
+                int l = result.Length;
+                for (int i = 0; i < 8 - l; i++)
+                {
+                    result = "0" + result;
+                }
+                return result;
+            }
+            return "el..tad";
+        }
     }
 }
--- ConvDecToBin/Program.cs
+++ ConvDecToBin/Program.cs
@@ -10,6 +10,15 @@ namespace ConvDecToBin
     {
         static void Main(string[] args)
         {
+            Console.Write("Kérek egy számot 0-225: ");
+            string s = Console.ReadLine();
+            Convert cv = new Convert
+            {
+                Dec = s
+            };
+            
+            Console.WriteLine(cv.Dec);
+            Console.ReadKey();
         }
     }
 }
